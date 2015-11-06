e5ba2113416c88a2d04a751197f5c25159ede62f
cstylianos <c.stylianos@gmail.com>
2015-11-05 21:33:23 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# script to execute python files "tweets_cleaned.py" and "average_degree.py"
+# located in ./src folder
+
+python ./src/tweets_cleaned.py
+python ./src/average_degree.py
